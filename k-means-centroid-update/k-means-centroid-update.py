ad1c1e853c9945038cb1e0e3a157cee8bf032260
jhshi1879 <jhshi1879@gmail.com>
2026-02-21 22:54:02 -0800

    Update solution for k-means-centroid-update

--- k-means-centroid-update/k-means-centroid-update.py
+++ k-means-centroid-update/k-means-centroid-update.py
@@ -5,7 +5,7 @@ def k_means_centroid_update(points, assignments, k):
   Compute new centroids as the mean of assigned points.
   """
   # Write code here
-  p = np.asarray(points, dtype=float)
+  p = np.asarray(points, dtype=float) 
   k_p = np.asarray(assignments, dtype=int) # (n_p, )
 
   if p.ndim == 1:
@@ -13,9 +13,17 @@ def k_means_centroid_update(points, assignments, k):
 
   n_p, d = p.shape
 
-  c = np.zeros((k, d))
-  for i in range(k):
-    c[i] = np.mean(p[k_p==i], axis=0)
+  # c = np.zeros((k, d))
+  # for i in range(k):
+  #   c[i] = np.mean(p[k_p==i], axis=0)
+
+  k_p_one_hot = np.eye(k)[k_p] # (n_p, k)
+
+  sums = k_p_one_hot.T @ p # (k d)
+  counts = np.sum(k_p_one_hot, axis=0) # (k, )
+
+  eps = 1e-15
+  c = sums / (counts+eps)[:, None]
 
   return c.tolist()
   
