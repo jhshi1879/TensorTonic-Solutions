9ba44ee56ce9730c0c54d9ba68ba8ee08c025992
jhshi1879 <jhshi1879@gmail.com>
2026-02-28 00:23:54 -0800

    Update solution for binning

--- binning/binning.py
+++ binning/binning.py
@@ -7,5 +7,7 @@ def binning(values, num_bins, eps=1e-15):
   # Write code here
   x = np.asarray(values) # (n,)
   w = (np.max(x) - np.min(x)) / (num_bins + eps)
+  # nodes = np.linspace(np.min(x), np.max(x), num_bins+1)
   bins = np.minimum(np.floor((x - np.min(x))/(w + eps)), np.ones_like(x) * (num_bins - 1))
+  # bins = np.digitize(x, nodes[1:-1])
   return bins.tolist()
